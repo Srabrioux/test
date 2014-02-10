b13b1eb9765bc125087d116ecead63b2a74c5090
Srabrioux <samuel.rabrioux@iut-valence.fr>
2014-02-10 10:33:35 +0100

    Version initial des fichiers.
    Création d'un projet eclipse pour exemple de la lampe.

---
+++ src/Bulb.java
@@ -0,0 +1,24 @@
+
+public class Bulb
+{
+	private static final int DEFAULT_POWER = 100 ;  //attributs
+	private final int power ;
+	private boolean isLit;
+	
+	public Bulb()
+	{
+		this.power= DEFAULT_POWER; //constructeurs
+		this.isLit = false; 
+	}
+	
+	
+	public void switchon()
+	{
+		
+	}
+	public void switchoff()
+	{	
+		
+	}
+
+}
