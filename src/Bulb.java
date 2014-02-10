ce5f1610030708fdce6d293b234d690a4b55220e
Srabrioux <samuel.rabrioux@iut-valence.fr>
2014-02-10 11:30:06 +0100

    Ajout de la doc

--- src/Bulb.java
+++ src/Bulb.java
@@ -1,24 +1,45 @@
 
+/**
+ * La lampe est caractérisée par sa puissance et son état.
+ */
 public class Bulb
 {
+	/**
+	 * La puissance par default est 100W.
+	 */
 	private static final int DEFAULT_POWER = 100 ;  //attributs
+	/**
+	 * Donne la puissance de la lampe.
+	 */
 	private final int power ;
+	/**
+	 * Donne l'état de la lampe (allumer/eteind)
+	 */
 	private boolean isLit;
 	
+	/**
+	 * Construit la lampe.
+	 */
 	public Bulb()
 	{
 		this.power= DEFAULT_POWER; //constructeurs
 		this.isLit = false; 
 	}
 	
-	
+	/**
+	 * Allume la lampe.
+	 */
 	public void switchon()
 	{
-		
+		this.isLit = true;
 	}
+	
+	/**
+	 * Eteind la lampe.
+	 */
 	public void switchoff()
 	{	
-		
+		this.isLit = false;
 	}
 
 }
